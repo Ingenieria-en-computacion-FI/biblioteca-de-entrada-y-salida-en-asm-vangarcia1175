d2c8f4b65656b3a6500805a7590fb4c3f6da56d2
Iván García <ivangarciaa1175@gmail.com>
2026-03-11 22:37:32 -0600

    Completar scan_string
    Se agrego la llamada al sistema read para leer varios bytes desde el teclado y guardarlos en el buffer indicado, despues se busca el salto de linea al final y se reemplaza por un terminador 0 para que la cadena sea valida

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -16,7 +16,22 @@ scan_string:
     ; TODO:
     ; 1. syscall read
     ; 2. guardar en buffer
+    mov ecx, eax        ; ECX = direccion del buffer
+    mov edx, ebx        ; EDX = tamaño maximo
+    mov eax, 3          ; syscall read
+    mov ebx, 0          ; fd = stdin
+    int 0x80
+
     ; 3. agregar terminador 0
+    add ecx, eax        ; apuntar al final de lo leido
+    dec ecx             ; retroceder al ultimo byte
+    cmp byte [ecx], 0x0A 
+    je  .poner_nulo
+    inc ecx             ; avanzar al siguiente
+
+.poner_nulo:
+    mov byte [ecx], 0   ; terminador nulo
+
 
     mov esp, ebp
     pop ebp
