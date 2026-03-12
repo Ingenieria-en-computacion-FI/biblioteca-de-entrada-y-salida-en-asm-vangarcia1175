1ddc219aa469236537b2aeb1a167a8db937ed4a2
Iván García <ivangarciaa1175@gmail.com>
2026-03-12 13:36:08 -0600

    Restaurar scan_char
    Se agrego una segunda llamada a read para consumir el salto de linea que queda en el buffer despues de leer el caracter, esto evita que scan_string lo tome como entrada

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -18,17 +18,25 @@ scan_char:
     ; TODO:
     ; 1. usar syscall read
     ; 2. leer 1 byte desde stdin
-.leer:
+
     mov eax, 3          ; syscall read
     mov ebx, 0          ; fd = stdin
     mov ecx, char_buffer ; direccion del buffer
     mov edx, 1          ; leer 1 byte
     int 0x80
-    cmp byte [char_buffer], 0x0A
-    je .leer
+    
+    
     ; 3. devolverlo en AL
     mov al, [char_buffer]
 
+    ; 4. consumir el '\n' que queda en el buffer
+    push eax
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, char_buffer
+    mov edx, 1
+    int 0x80
+    pop eax
 
     mov esp, ebp
     pop ebp
