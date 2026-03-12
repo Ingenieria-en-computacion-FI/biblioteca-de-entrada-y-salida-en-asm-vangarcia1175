45de5066679e44257b6c1b2c6d8b53e83db9692a
Iván García <ivangarciaa1175@gmail.com>
2026-03-11 22:24:56 -0600

    Completar print_string
    Se agrego un loop que recorre la cadena byte por byte hasta encontrar el terminador 0 para calcular su longitud, luego se usa la llamada al sistem write para imprimir toda la cadena de una sola vez

--- src/print_string.asm
+++ src/print_string.asm
@@ -1,21 +1,31 @@
 SECTION .text
 global print_string
-
 ; ---------------------------------
 ; print_string
 ; Entrada:
 ;   EAX = direccion de cadena
 ;   cadena terminada en 0
 ; ---------------------------------
-
 print_string:
-
     push ebp
     mov ebp, esp
 
     ; TODO:
     ; 1. calcular longitud
+    mov ecx, eax        ; ECX = puntero al inicio de la cadena
+    mov edx, 0          ; EDX = contador de longitud
+.loop:
+    cmp byte [ecx + edx], 0     
+    je  .escribir
+    inc edx
+    jmp .loop
+
+.escribir:
     ; 2. syscall write
+    ; write(fd=1, cadena, longitud)
+    mov ebx, 1          ; fd = stdout
+    mov eax, 4          ; syscall write
+    int 0x80
 
     mov esp, ebp
     pop ebp
