3c79d85c0d90150531476c3effbe2ae982757eac
Iván García <ivangarciaa1175@gmail.com>
2026-03-12 07:46:16 -0600

    scan_char ignora saltos de linea
    Se agrega un loop para saltar los saltos de linea que puedan quedar en el buffer antes del caracter real

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -18,12 +18,14 @@ scan_char:
     ; TODO:
     ; 1. usar syscall read
     ; 2. leer 1 byte desde stdin
+.leer:
     mov eax, 3          ; syscall read
     mov ebx, 0          ; fd = stdin
     mov ecx, char_buffer ; direccion del buffer
     mov edx, 1          ; leer 1 byte
     int 0x80
-
+    cmp byte [char_buffer], 0x0A
+    je .leer
     ; 3. devolverlo en AL
     mov al, [char_buffer]
 
