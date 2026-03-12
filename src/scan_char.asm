16653078cbe8b8b830db3b0b23aeb70dbe48d097
Iván García <ivangarciaa1175@gmail.com>
2026-03-11 23:25:07 -0600

    Revertir scan_char
    Se elimina la lectura extra del salto de linea porque los tests automaticos manejan la entrada de forma diferente

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -27,14 +27,6 @@ scan_char:
     ; 3. devolverlo en AL
     mov al, [char_buffer]
 
-    ; 4. consumir el '\n' que queda en el buffer
-    push eax
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, char_buffer
-    mov edx, 1
-    int 0x80
-    pop eax
 
     mov esp, ebp
     pop ebp
