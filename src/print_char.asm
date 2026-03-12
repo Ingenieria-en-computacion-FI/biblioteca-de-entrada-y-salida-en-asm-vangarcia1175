c5b0c75a8ad5b8f32cb95727aca605d5565405b1
Iván García <ivangarciaa1175@gmail.com>
2026-03-11 22:16:43 -0600

    Completar print_char
    Se agrego una seccion .bss para reservar 1 byte en memoria, se guarda el caracter de AL en ese espacio y se usa la llamada al sistema write para imprimirlo en pantalla

--- src/print_char.asm
+++ src/print_char.asm
@@ -1,3 +1,6 @@
+SECTION .bss
+    buffer_char resb 1          ; buffer de 1 byte para el caracter
+
 SECTION .text
 global print_char
 
@@ -14,10 +17,18 @@ print_char:
 
     ; TODO:
     ; 1. Guardar el caracter en memoria
+    mov [buffer_char], al
+
     ; 2. Usar syscall write
     ; 3. Imprimir 1 byte
 
     ; write(fd=1, buffer, 1)
+
+    mov eax, 4                  ; syscall write
+    mov ebx, 1                  ; fd = stdout
+    mov ecx, buffer_char        ; direccion del buffer
+    mov edx, 1                  ; longitud = 1 byte
+    int 0x80
 
     mov esp, ebp
     pop ebp
