3a5a4137f2d353a245d6835f66750de53afc461c
Iván García <ivangarciaa1175@gmail.com>
2026-03-11 22:30:07 -0600

    Completar scan_char
    Se agrego la llamada al sistema read para leer 1 solo byte desde el teclado (stdin), ese byte se guarda en un buffer de memoria y luego se copia al registro AL para devolverlo como resultado

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -18,7 +18,14 @@ scan_char:
     ; TODO:
     ; 1. usar syscall read
     ; 2. leer 1 byte desde stdin
+    mov eax, 3          ; syscall read
+    mov ebx, 0          ; fd = stdin
+    mov ecx, char_buffer ; direccion del buffer
+    mov edx, 1          ; leer 1 byte
+    int 0x80
+
     ; 3. devolverlo en AL
+    mov al, [char_buffer]
 
     mov esp, ebp
     pop ebp
